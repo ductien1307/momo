f5622b26335dcd153306d4fefdd37746257da438
ductien1307 <ductien1307@gmail.com>
2022-06-24 12:39:32 +0700

    Update CapabilitiesManager.java

--- src/main/java/driver/CapabilitiesManager.java
+++ src/main/java/driver/CapabilitiesManager.java
@@ -2,6 +2,7 @@ package driver;
 
 
 //import config.ReadProperties;
+
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -23,7 +24,7 @@ public class CapabilitiesManager {
                 //cap.setCapability("deviceName", readProperties.deviceNameAndroid());
                 //cap.setCapability("appPackage", readProperties.appPackage());
                 //cap.setCapability("appActivity", readProperties.appActivity());
-                cap.setCapability("app", "/Users/tiennguyen/Desktop/momo/momo/apps/com.mservice.momotransfer_2022-05-18.apk");
+                cap.setCapability("app", System.getProperty("user.dir") + "/apps/com.mservice.momotransfer_2022-05-18.apk");
                 //cap.setCapability("app", System.getProperty("user.dir") + "/apps/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
                 break;
             case "ios":
