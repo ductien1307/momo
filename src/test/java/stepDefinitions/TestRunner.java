1b2ce7543d39f39f2079fbce55b4f1acd78720a4
ductien1307 <ductien1307@gmail.com>
2022-06-24 15:17:40 +0700

    Update TestRunner.java

--- src/test/java/stepDefinitions/TestRunner.java
+++ src/test/java/stepDefinitions/TestRunner.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
+/*
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = { "stepDefinitions" },
@@ -13,16 +14,16 @@ import org.junit.runner.RunWith;
                 , "html:target/cucumber"
                 , "summary"
                 , "json:target/cucumber.json"},
-        tags = "not @ignore")
+        tags = "not @ignore")*/
 
-/*@CucumberOptions(
+@CucumberOptions(
         features = "src/test/resources/features",
         glue = { "stepDefinitions" },
         plugin = {"pretty"
                 , "html:target/cucumber"
                 , "summary"
                 , "de.monochromata.cucumber.report.PrettyReports:target/cucumber-reports"},
-        tags = "not @ignore")*/
+        tags = "not @ignore")
 
 public class TestRunner {
     //System.out.println("XXX");
